4c662ee1c93f4424511854cec14c353d4bf7864b
Ulisses Gonçalves <ulissesmkt2104@gmail.com>
2023-12-08 22:57:52 -0300

    Desafio concluído

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -4,11 +4,18 @@ namespace DesafioPOO.Models
     {
         public string Numero { get; set; }
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        // IMPLENTADO: Seguindo o principio de que utilizar o protected permite somente que as instancias que vão erdar essa instancia possa fazer a alteração nos elementos.
+        protected string Modelo {get; set;}
+        protected string IMEI {get; set;}
+        protected int Memoria {get; set;}
 
-        public Smartphone(string numero)
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
             // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
         }
 
         public void Ligar()
--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+// IMPLEMENTADO
+
+Nokia nokia = new Nokia("(12)34567890", "Nokia 007", "fadacaba10", 1024);
+Iphone iphone = new Iphone("(09)87654321", "iPhone 17", "abacadapra11", 4096);
+
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Facebook");
+
+Console.WriteLine("----------------------------");
+
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Spotify");
